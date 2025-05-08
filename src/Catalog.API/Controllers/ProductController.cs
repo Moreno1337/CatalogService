cc5447ead309d64f7991ea5dff55752b3df44088
João Moreno <jmoreno@csicargo.com.br>
2025-05-08 15:16:21 -0300

    Product controller with basic functionality.

---
+++ src/Catalog.API/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ProductController : ControllerBase
+    {
+        private readonly IProductRepository _repository;
+
+        public ProductController(IProductRepository repository)
+        {
+            _repository = repository;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll() =>
+            Ok(await _repository.GetAllAsync());
+    }
+}
---
+++ src/Catalog.Infrastructure/Repositories/ProductRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.Extensions.Configuration;
+using Catalog.Domain.Entities;
+using Catalog.Domain.Interfaces;
+using MongoDB.Driver;
+
+namespace Catalog.Infrastructure.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IMongoCollection<Product> _collection;
+
+        public ProductRepository(IConfiguration config)
+        {
+            var client = new MongoClient(config["MongoSettings:ConnectionString"]);
+            var database = client.GetDatabase(config["MongoSettings:Database"]);
+            _collection = database.GetCollection<Product>("Products");
+        }
+
+        public async Task<IEnumerable<Product>> GetAllAsync() =>
+            await _collection.Find(_ => true).ToListAsync();
+
+        public async Task<Product?> GetByIdAsync(string id) =>
+            await _collection.Find(p => p.Id == id).FirstOrDefaultAsync();
+
+        public async Task AddAsync(Product product) =>
+            await _collection.InsertOneAsync(product);
+
+        public async Task UpdateAsync(Product product) =>
+            await _collection.ReplaceOneAsync(p => p.Id == product.Id, product);
+
+        public async Task DeleteAsync(string id) =>
+            await _collection.DeleteOneAsync(p => p.Id == id);
+    }
+}
